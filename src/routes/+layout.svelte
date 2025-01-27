8cf495409fa2df4bcfc7e4ee97c3e32dc4a5b7fc
Nate Brune <natebrune@protonmail.com>
2025-01-27 06:02:51 +0000

    Assistant checkpoint: Added header with home button to layout component
    Assistant generated file changes:
    - src/routes/+layout.svelte: Add header with home button
    
    ---
    
    User prompt:
    
    lets add a header that just says InfoXM with a button to go home on all pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,17 @@
+
 <script>
 	import "./styles.css";
+  import { goto } from '$app/navigation';
 </script>
+
+<header class="navbar bg-base-100 shadow-lg">
+  <div class="flex-1">
+    <a href="/" class="btn btn-ghost normal-case text-xl">InfoXM</a>
+  </div>
+  <div class="flex-none">
+    <a href="/" class="btn btn-ghost">Home</a>
+  </div>
+</header>
 
 <div class="app">
 	<main>
